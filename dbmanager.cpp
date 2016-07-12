89f67e0be7cb06d1dd6bccdd5ad9ecc857d1275d
Neal Nguyen <nealnguyen@knights.ucf.edu>
2016-07-12 18:30:36 -0400

    login connected to database

--- dbmanager.cpp
+++ dbmanager.cpp
@@ -5,14 +5,13 @@ DbManager::DbManager(const QString & path)
 {
    m_db = QSqlDatabase::addDatabase("QSQLITE");
    m_db.setDatabaseName(path);
-
    if (!m_db.open())
    {
       qDebug() << "Error: connection with database fail";
    }
    else
    {
-      qDebug() << "Database: connection ok";
+
    }
 }
 bool DbManager::addPerson(const QString& name)
@@ -20,7 +19,7 @@ bool DbManager::addPerson(const QString& name)
    bool success = false;
    // you should check if args are ok first...
    QSqlQuery query;
-   query.prepare("INSERT INTO people (name) VALUES (:name)");
+   query.prepare("INSERT INTO employee (first) VALUES (:name)");
    query.bindValue(":name", name);
    if(query.exec())
    {
@@ -33,4 +32,29 @@ bool DbManager::addPerson(const QString& name)
    }
 
    return success;
+}
+
+bool DbManager::pinChecker(QString& pin)
+{
+    bool success = false;
+    QSqlQuery query;
+    query.prepare("SELECT PIN FROM employee");
+    if(query.exec())
+    {
+        while(query.next() )
+        {
+            if(pin.compare(query.value(0).toString()) == 0)
+            {
+                success = true;
+                break;
+            }
+        }
+        m_db.close();
+        m_db.removeDatabase(m_db.connectionName());
+        return success;
+    }
+    else
+    {
+            qDebug() << "Error" << query.lastError();
+    }
 }
