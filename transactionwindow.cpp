a480d2094ef8ca5a19103257bd22ec2a4e3a9b6e
MiguelAleksich <miguelaleksich@live.com>
2016-07-17 14:45:26 -0400

    fixed delete on transaction window

--- transactionwindow.cpp
+++ transactionwindow.cpp
@@ -1,14 +1,142 @@
 #include "transactionwindow.h"
 #include "ui_transactionwindow.h"
+#include <dbmanager.h>
+#include <QMessageBox>
+#include <QDebug>
+
+
+
+float subTotal;
+float grandTotal;
+float salesTax = .06;
+float taxTotal;
+
+static double extractDouble(const QString &s);
 
 TransactionWindow::TransactionWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::TransactionWindow)
 {
     ui->setupUi(this);
+
+    DbManager *dbmanager = new DbManager();
+
+    this->model = new QSqlQueryModel();
+    model->setQuery("SELECT [name], [price] FROM [main].[inventory]");
+
+     ui->tableView->setModel(model);
+
 }
 
 TransactionWindow::~TransactionWindow()
 {
     delete ui;
+}
+
+void TransactionWindow::on_tableView_activated(const QModelIndex &index)
+{
+    int row = index.row();
+//    int val = ui->tableView->model()->data(index).toInt();
+    float val = index.sibling(row, 1).data().toFloat();
+    QString item = index.sibling(row,0).data().toString();
+
+    qDebug()<< "index is: " <<index;
+    qDebug()<< "row is: " <<row;
+
+//    QSqlQuery qry;
+
+//        qry.prepare("SELECT * FROM employee WHERE first='"+val+"' or last='"+val+"' ");
+//        if(qry.exec())
+//        {
+//            while(qry.next())
+//            {
+
+//                ui->lineEdit_pin->setText(qry.value(5).toString());
+
+//                ui->lineEdit_address->setText(qry.value(3).toString());
+//                ui->lineEdit_email->setText(qry.value(6).toString());
+//                ui->lineEdit_phone->setText(qry.value(4).toString());
+
+//            }
+
+//            selectedName = ui->tableView->model()->data(index).toString();
+
+
+//        }
+
+    ui->listWidget->addItem(item + " \t" + QString::number(val));
+  //  ui->listWidget->addItem(QString::number(item));
+
+
+    subTotal = subTotal + val;
+    taxTotal = subTotal * salesTax;
+    taxTotal = floor(taxTotal*100+0.5)/100;
+
+
+    grandTotal = subTotal + taxTotal;
+    grandTotal = floor(grandTotal*100+0.5)/100;
+
+
+
+    qDebug()<<"Sub Total is: " <<subTotal;
+    qDebug()<< "Tax Total is: " <<taxTotal;
+    qDebug()<< "Grand Total is: " <<grandTotal;
+    ui->lineEdit_subTotal->setText(QString::number(subTotal));
+    ui->lineEdit_2_tax->setText(QString::number(taxTotal));
+    ui->lineEdit_3_grandTotal->setText(QString::number(grandTotal));
+}
+
+void TransactionWindow::on_pushButton_clicked()
+{
+   // ui->listWidget->currentItem()->;
+   QString str = ui->listWidget->currentItem()->text();
+   qDebug()<< "str is" <<str;
+
+//   for(int i=0;i<val.length();i++)
+//   {
+//       if val.
+//   }
+   float val;
+   val = extractDouble(str);
+
+    qDebug()<< "val is" <<val;
+   subTotal = subTotal - val;
+   taxTotal = subTotal * salesTax;
+   taxTotal = floor(taxTotal*100+0.5)/100;
+
+   grandTotal = subTotal + taxTotal;
+   grandTotal = floor(grandTotal*100+0.5)/100;
+
+   if(subTotal<.5)
+   {
+       subTotal = 0;
+       taxTotal = 0;
+       grandTotal = 0;
+   }
+   ui->lineEdit_subTotal->setText(QString::number(subTotal));
+   ui->lineEdit_2_tax->setText(QString::number(taxTotal));
+   ui->lineEdit_3_grandTotal->setText(QString::number(grandTotal));
+    qDeleteAll(ui->listWidget->selectedItems());
+
+
+     qDebug()<< "Sub Total is: " <<subTotal;
+     qDebug()<< "Tax Total is: " <<taxTotal;
+     qDebug()<< "Grand Total is: " <<grandTotal;
+}
+
+static double extractDouble(const QString &s)
+{
+    QString num;
+    foreach(QChar c, s) {
+        if (c.isDigit() || c == '.' || c == '-') {
+            num.append(c);
+        }
+    }
+    bool ok;
+    double ret = num.toDouble(&ok);
+    if (ok) {
+        return ret;
+    } else {
+        throw "Cannot extract double value";
+    }
 }
--- transactionwindow.h
+++ transactionwindow.h
@@ -2,6 +2,8 @@
 #define TRANSACTIONWINDOW_H
 
 #include <QWidget>
+#include <QDialog>
+#include <QtSql>
 //trans test 2
 //trans test 3
 namespace Ui {
@@ -16,8 +18,14 @@ public:
     explicit TransactionWindow(QWidget *parent = 0);
     ~TransactionWindow();
 
+private slots:
+    void on_tableView_activated(const QModelIndex &index);
+
+    void on_pushButton_clicked();
+
 private:
     Ui::TransactionWindow *ui;
+    QSqlQueryModel *model;
 };
 
 #endif // TRANSACTIONWINDOW_H
